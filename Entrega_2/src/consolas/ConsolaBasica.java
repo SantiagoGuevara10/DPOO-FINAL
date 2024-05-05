da929b448a7d05f5f7cb1b33cb6aa734e3149b93
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2024-05-05 11:53:53 -0500

    guardar

---
+++ Entrega_2/src/consolas/ConsolaBasica.java
@@ -0,0 +1,5 @@
+package consolas;
+
+public class ConsolaBasica {
+
+}
---
+++ Entrega_2/src/consolas/ConsolaPrincipal.java
@@ -0,0 +1,10 @@
+package consolas;
+
+/**
+ * En esta clase se encuentra el método main de la aplicación.
+ *//**
+ * En esta clase se encuentra el método main de la aplicación.
+ */
+public class ConsolaPrincipal {
+
+}
