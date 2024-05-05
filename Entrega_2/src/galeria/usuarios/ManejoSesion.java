4fd1e7d6bb8142e0d1a98a33258b7faac889aa58
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2024-05-05 11:19:37 -0500

    commit

---
+++ Entrega_2/src/galeria/usuarios/ManejoSesion.java
@@ -0,0 +1,56 @@
+package galeria.usuarios;
+ 
+import galeria.usuarios.Empleado;
+import galeria.usuarios.FileUtils;
+import galeria.usuarios.CompradorPropietario;
+ 
+ 
+public class ManejoSesion {
+    private static Empleado empleadoActual;
+    private static CompradorPropietario compradorPropietarioActual;
+ 
+    public static void loginEmpleado(String username, String password) {
+        UsuariosRegistrados usuariosRegistrados = new UsuariosRegistrados();
+        if (FileUtils.verifyUser(username, password)) {
+            for (Empleado empleado : usuariosRegistrados.getUsuariosEnPrograma()) {
+                if (empleado.getUsername().equals(username)) {
+                    empleadoActual = empleado;
+                    
+                    return;
+                }
+            }
+           
+        } else {
+            
+        }
+    }
+ 
+    public static void loginCompradorPropietario(String username, String password) {
+        UsuariosRegistrados usuariosRegistrados = new UsuariosRegistrados();
+        if (FileUtils.verifyUser(username, password)) {
+            for (CompradorPropietario comprador : usuariosRegistrados.getCompradoresEnPrograma()) {
+                if (comprador.getUsername().equals(username)) {
+                    compradorPropietarioActual = comprador;
+                  
+                    return;
+                }
+            }
+            
+        } else {
+           
+        }
+    }
+ 
+    public static Empleado getEmpleadoActual() {
+        return empleadoActual;
+    }
+ 
+    public static CompradorPropietario getCompradorPropietarioActual() {
+        return compradorPropietarioActual;
+    }
+ 
+    public static void logout() {
+        empleadoActual = null;
+        compradorPropietarioActual = null;
+    }
+}
